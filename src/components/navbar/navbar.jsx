475040e504c4d9f706df498021a5aca5861fb349
Diego <diegovidela_93@hotmail.com>
2023-11-07 23:41:31 -0300

    se modifica logo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { LinksContainer, NavbarContainer } from "./styles";
+import img from './img-logo-empresa.png';
+import imgcarrito from './img-carrito.jpg';
 
 
 export const Navbar=()=>{
     return(
         <NavbarContainer>
             <div>
-                <img src="./src/assets/img-logo-empresa.png" alt="Logo Empresa" />
+                <img src= {img} alt="Logo Empresa" />
             </div>
             <LinksContainer>
                 <a href="">Productos</a>
                 <a href="">Iniciar Session</a>
-                <img src="./src/assets/img-carrito.jpg" alt="" />
+                <img src= {imgcarrito} alt="" />
             </LinksContainer>
         </NavbarContainer>
     )
